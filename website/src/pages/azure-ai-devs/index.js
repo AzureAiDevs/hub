41b46f8231513c6b2ce40ff4232530b5836c2141
Dave Glover <dglover@microsoft.com>
2023-03-14 12:04:06 +1100

    docs

--- website/src/pages/azure-ai-devs/index.js
+++ website/src/pages/azure-ai-devs/index.js
@@ -10,7 +10,7 @@ const CampaignFeatures = [
     title: '#30DaysOfAzureAI',
     Svg: require('@site/static/img/campaign/30days.svg').default,
     fillColor: "#f25022",
-    link: "azure-ai-devs/30Days",
+    link: "/hub/roadmap/30days",
     description: (
       <>
       Join us on a #30Day journey covering Azure OpenAI and Cognitive Services, Azure ML, MLOps, and AI App integration!
